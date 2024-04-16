bd9c1f81b1d4dcf31058e816edd108fc1a40296f
maf.consultoria <maf.consultoria@wefin.com.br>
2024-04-16 12:36:48 -0300

    feat(routes) add resolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { inject } from '@angular/core';
+import { ProductsService } from './shared/services/products.service';
 
 export const routes: Routes = [
   {
@@ -14,7 +16,16 @@ export const routes: Routes = [
       ),
   },
   {
-    path: 'edit-product',
+    path: 'edit-product/:id',
+    resolve: {
+      product: (route: ActivatedRouteSnapshot) => {
+        const productService = inject(ProductsService);
+
+        const productId = route.paramMap.get('id') as string;
+
+        return productService.get(productId);
+      },
+    },
     loadComponent: () =>
       import('./features/edit/edit.component').then((m) => m.EditComponent),
   },
