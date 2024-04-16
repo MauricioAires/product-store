d40a894ec3b519873b3b2845ad76990ab499038d
maf.consultoria <maf.consultoria@wefin.com.br>
2024-04-16 12:38:39 -0300

    feat(edit product) access product resolver and save update

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -9,9 +9,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-edit',
@@ -29,9 +30,10 @@ export class EditComponent {
   productService = inject(ProductsService);
   matSnackBar = inject(MatSnackBar);
   router = inject(Router);
+  product = inject(ActivatedRoute).snapshot.data['product'] as Product;
 
   form = new FormGroup({
-    title: new FormControl<string>('', {
+    title: new FormControl<string>(this.product.title, {
       nonNullable: true,
       validators: Validators.required,
     }),
@@ -39,9 +41,10 @@ export class EditComponent {
 
   onSubmit() {
     const { title } = this.form.value;
+    const { id } = this.product;
 
     this.productService
-      .create({
+      .update(id, {
         title: title!,
       })
       .subscribe(() => {
