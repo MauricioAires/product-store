4044ca7b12caa8024f2041789219fed2958c7d0f
maf.consultoria <maf.consultoria@wefin.com.br>
2024-04-16 09:15:05 -0300

    feat: add button new product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -15,7 +17,6 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.productsService.getAll().subscribe((res) => {
-      console.log(res);
       this.products = res;
     });
   }
