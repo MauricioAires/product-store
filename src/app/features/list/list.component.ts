3e58f2bd9fd8f4c43863a32aa2680ddc3fb0903e
maf.consultoria <maf.consultoria@wefin.com.br>
2024-04-16 12:38:17 -0300

    feat(products) update route edit

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -25,7 +25,7 @@ export class ListComponent implements OnInit {
     });
   }
 
-  onEdit() {
-    this.router.navigateByUrl('edit-product');
+  onEdit(id: string) {
+    this.router.navigateByUrl(`edit-product/${id}`);
   }
 }
